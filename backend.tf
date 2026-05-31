a316cfdece3f2276852d6aee03e03894442874e5
Bardoso244 <bart.rosiecki@gmail.com>
2026-05-31 08:34:28 +0100

    Removing prod env for testing

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "server-based-app1-bartr"
-    key = "prod/terraform.tfstate"
+    key = "terraform.tfstate"
     region = "eu-west-2"
     use_lockfile = true
     encrypt = true
